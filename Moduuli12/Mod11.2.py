376f4f09e190fee2be3e505da64a76c4bcb7df21
Hietalajani <hietsuj@gmail.com>
2022-11-01 11:12:16 +0200

    11 tehtävät tehty, 10.4 nimetty uudelleen sillä import ongelmat

---
+++ Moduuli12/Mod11.2.py
@@ -0,0 +1,17 @@
+import json
+import requests
+
+
+
+paik = input('Anna paikkakunnan nimi: ')
+
+API = 'aa58881d67d7b5251b15873ce3750c23'
+
+geocode = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={paik}&limit=5&appid={API}').json()
+
+lat = geocode[0]['lat']
+lon = geocode[0]['lon']
+
+weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API}').json()
+
+print(f'Kaupungin {paik}\nSäätila: {weather["weather"][0]["main"]}\nLämpötila: {weather["main"]["temp"]-273.15:.2f} Celsius-astetta')
