e0f0969073692dbf29888b2fafb0d318c6fab6f6
Hietalajani <hietsuj@gmail.com>
2022-11-09 18:46:42 +0200

    1.9 optimoitu

--- JSModuuli1/Mod1.9.js
+++ JSModuuli1/Mod1.9.js
@@ -1,8 +1,12 @@
 'use strict'
 const number = parseInt(prompt('GIMME A NUMBER DICKHEAD'))
 
-if (number % 2 !== 0) {
-    for (let i = 2; i <= Math.floor(Math.sqrt(number)+1); i++) {
+if (number === 2 || number === 3 || number === 5 || number === 7) {
+    document.getElementById('lol').innerHTML = `${number} is a prime number`
+}
+
+if (number % 2 !== 0 && number > 7) {
+    for (let i = 3; i <= Math.floor(Math.sqrt(number)+1); i += 2) {
         if (number % i === 0) {
             document.getElementById('lol').innerHTML = `${number} is not a prime number`
             break
